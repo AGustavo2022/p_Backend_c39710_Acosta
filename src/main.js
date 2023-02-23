acd53f892fc6a2af213f325fa06afc15ec5b2666
Acosta Gustavo <gustavo080487@gmail.com>
2023-02-23 12:01:17 -0300

    Manejo de Archivos

--- src/main.js
+++ src/main.js
@@ -90,20 +90,20 @@ console.log ('testing ...')
 
 const productManager = new ProductManager ('./static/productManager.txt')
 
-console.log(await productManager.getProduct())
+//console.log(await productManager.getProduct())
 
-await productManager.addProduct(new Producto('Producto Prueba','Este es un producto de prueba',200,'sin imagen','abc123',25))
-
-console.log(await productManager.getProduct())
-
-
-// await productManager.getProductById('c255951f-2918-41ba-b053-b6eef6f0fee2')
-
-//await productManager.deleteProductId('9748972a-ced0-420f-9509-958fe1449c8a')
+//await productManager.addProduct(new Producto('Producto Prueba','Este es un producto de prueba',200,'sin imagen','abc123',25))
 
 //console.log(await productManager.getProduct())
 
-//await productManager.updateProduct('ab4bbcc5-0bfa-4ef4-b302-81039fec045d','Producto Prueba4','Este es un producto de prueba2',300,'sin imagen','abc124',35)
+//await productManager.getProductById('6bac27d8-f732-415c-8b61-e34109e8a620')
+
+//await productManager.updateProduct('6bac27d8-f732-415c-8b61-e34109e8a620','Producto Prueba','Este es un producto de prueba2',300,'sin imagen','abc124',40)
+
+//await productManager.deleteProductId('6bac27d8-f732-415c-8b61-e34109e8a620')
+
+console.log(await productManager.getProduct())
+
 
 
 
