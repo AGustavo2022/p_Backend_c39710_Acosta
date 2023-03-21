4b344d4aa380bb38604a09a7dd4392062f3f40b4
Acosta Gustavo <gustavo080487@gmail.com>
2023-03-21 08:26:00 -0300

    motores de plantillas + websocket

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 import  express  from "express";
-import { apiRouter } from "./routers/apiRouter.js";
+import { apiRouter } from "./routers/apiRouter.js"
+import { viewsRouter } from "./routers/viewsRouter.js"
+import { engine } from 'express-handlebars'
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+
+app.engine('handlebars', engine())
+app.set('views', './views')
+app.set('view engine', 'handlebars')
+
+app.use(express.static('./public'))
+
 app.use('/api', apiRouter)
+app.use('/', viewsRouter)
+
 
 app.listen(8080, ()=>console.log('!Servidor arriba en el puerto 8080!'))
 
---
+++ src/routers/viewsRouter.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { ProductManager } from "../productManager.js"
+
+export const viewsRouter = Router()
+
+
+
+
+// viewsRouter.get('/', async (req, res) => {
+//     const mensajes = await mensajesManager.buscarCosas()
+//     res.render('mensajes', {
+//         pageTitle: 'Chat'
+//     })
+// })
