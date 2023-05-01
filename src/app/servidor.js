1cce8ac398b0cc9cc4cbad836565802ef31a774f
Acosta Gustavo <gustavo080487@gmail.com>
2023-05-01 17:22:01 -0300

    segundaPreEntega

--- src/app/servidor.js
+++ src/app/servidor.js
@@ -1,13 +1,10 @@
 import  express  from "express";
-import { PORT } from '../config/servidor.config.js'
 import { engine } from 'express-handlebars'
 import { apiRouter } from "../routers/apiRouter.js"
 import { viewsRouter } from "../routers/viewsRouter.js"
-import { conectar } from "../database/mongoose.js";
 
-await conectar()
 
-const app = express();
+export const app = express();
 
 
 app.use(express.static('public'))
@@ -19,5 +16,3 @@ app.set('view engine', 'handlebars')
 app.use('/api', apiRouter)
 app.use('/', viewsRouter)
 
-app.listen(PORT, ()=>console.log(`!Servidor arriba en el puerto ${PORT}!`))
-    
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-import './app/servidor.js'
+import  { app } from './app/servidor.js'
+import { PORT } from './config/servidor.config.js'
+import { conectar } from "./database/mongoose.js"
+
+
+await conectar()
+
+app.listen(PORT, ()=>console.log(`!Servidor arriba en el puerto ${PORT}!`))
